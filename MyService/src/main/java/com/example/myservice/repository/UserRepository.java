da61a2508a922d90dcbacf16373745463554b21e
Graniti <84074405+Granitasse@users.noreply.github.com>
2025-04-21 15:37:21 +0200

    after activing intellegency

--- MyService/src/main/java/com/example/myservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myservice.repository;
-
-import com.example.myservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> getUserByUsername(String username);
-}
